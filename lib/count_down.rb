1abb966f7f9c71560be0132885154bd395a8522c
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-05 14:31:25 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,8 @@
+magic_exit_number = -1
 count = 10
-while count < 10 do
+while count < 11 do
+  break if count == magic_exit_number
   puts #{count}
+  puts "Happy New Year!"
   count = count - 1
 end
